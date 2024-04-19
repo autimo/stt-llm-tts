1827f65ee857a603229f797cd6eced6302e59e1d
Warren Uniewski <warren@autimo.com>
2024-04-18 17:00:00 -0700

    v1

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # GCP Generative AI Demo
 
+This repository contains the code and resources for a demonstration of Generative AI capabilities on Google Cloud Platform (GCP). It showcases various AI-driven applications, including speech-to-text, text-to-speech, and AI content generation.
+
 ## Prerequisites
 
 ```shell
 sudo apt-get install portaudio19-dev
 pip install -r requirements.txt
+```
+
+## Running the demo
+
+```shell
+python main.py
 ```
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import struct
-import time
 from collections import deque
 from datetime import datetime
 
-import cv2
 import google.generativeai as genai
-import PIL.Image
 import pvporcupine
 import pyaudio
+import webrtcvad
 from dotenv import load_dotenv
 from google.cloud import speech, texttospeech
 from google.generativeai.types.generation_types import BlockedPromptException
@@ -18,9 +16,7 @@ from pydub.playback import play
 load_dotenv()
 
 cloud_summit_information = """
-Here is the information from the PDF presented in markdown format:
-
-# Vancouver's Largest Cloud Event
+# Vancouver Cloud Summit 2024 
 
 ## All the Clouds, All at once
 
@@ -37,35 +33,42 @@ Whether you are a developer, IT professional, business leader, or simply curious
 
 ### Community Speakers
 
-**Payam Moghad**
-*Talk: Develop Directly in the Cloud*
-Payam has talked recently at DevOps Days, Hashicorp, AWS Meetup and more.
-
 **Mattias Andersson**
 *Talk: Multi-Cloud and Serverless - Good or Bad?*
+*Time: 10:50 am*
 Mattias has talked at Microsoft Inspire, Microsoft Tech Skills Day, 100+ Training Videos for Pluralsight and A Cloud Guru, and much more.
+
+**Payam Moghad**
+*Talk: Develop Directly in the Cloud*
+*Time: 12:30 pm*
+Payam has talked recently at DevOps Days, Hashicorp, AWS Meetup and more.
 
 **Amit Bajaj**
 *Talk: Optimize your Cloud for Cost & Sustainability*
+*Time: 1:50 pm*
 Amit has talked at AWS Re:Invent, AWS OnAir Podcast and more.
 
 ### Guest Speakers
 
-**Jeff Price**
-*Talk: Leveling the Cloud Playing Field: Diversification Made Easy*
-Jeff is a Technologist, Strategist, Futurist, and Communicator.
-
-**Chester Wisniewski**
-*Talk: Cloud Security Strategies for 2024 and Beyond*
-Based in Vancouver, Chester regularly speaks at industry events, including RSA Conference, Virus Bulletin, Security BSides (Vancouver, London, Wales, Perth, Austin, Detroit, Los Angeles, Boston, and Calgary) and others. He's widely recognized as one of the industry's top security researchers and is regularly consulted by press, appearing on BBC News, ABC, NBC, Bloomberg, Washington Post, CBC, NPR, and more.
-
 **Julia Furst Morgado**
 *Talk: Head in the Clouds, Feet on the Ground: Practical Cloud Migration for Business Continuity*
+*Time: 10:10 am*
 Julia is a recognized leader in the tech community, serving as an AWS Community Builder, a CNCF Ambassador, a Google Women Techmakers Ambassador, and a Girl Code Ambassador. She actively supports and organizes the NY Code & Coffee Meetup and the KubeHuddle conference, fostering collaboration and learning opportunities.
 
 **Tyler Mitchell**
 *Talk: Future-Ready: Database Strategies for AI Innovation*
+*Time: 11:30 am*
 Tyler shares his insights from decades of experience in handling large datasets, exploring distributed data technology, and building solutions to extract value from disparate big data lakes. His role at Couchbase is to share the vision for how developers and architects can future-proof their work and take advantage of next-generation technology, today. Tyler is an O'Reilley author, and spent much of his time working in British Columbia for technology end natural resource companies. His early background is in geospatial technology and open source software advocacy.
+
+**Chester Wisniewski**
+*Talk: Cloud Security Strategies for 2024 and Beyond*
+*Time: 1:10 pm*
+Based in Vancouver, Chester regularly speaks at industry events, including RSA Conference, Virus Bulletin, Security BSides (Vancouver, London, Wales, Perth, Austin, Detroit, Los Angeles, Boston, and Calgary) and others. He's widely recognized as one of the industry's top security researchers and is regularly consulted by press, appearing on BBC News, ABC, NBC, Bloomberg, Washington Post, CBC, NPR, and more.
+
+**Jeff Price**
+*Talk: Leveling the Cloud Playing Field: Diversification Made Easy*
+*Time: 2:30 pm*
+Jeff is a Technologist, Strategist, Futurist, and Communicator.
 
 ## Schedule
 
@@ -113,17 +116,30 @@ Orpheum
 """
 
 
-def record_audio(stream, rate, frame_length, record_seconds):
+def record_audio(audio_stream, sample_rate, frame_length, silent_seconds_required=1.5):
     print("Recording...")
     frames = []
-    for _ in range(0, int(rate / frame_length * record_seconds)):
-        try:
-            data = stream.read(frame_length, exception_on_overflow=False)
-            frames.append(data)
-        except IOError as e:
-            if e.errno == pyaudio.paInputOverflowed:
-                # Handling overflow
-                continue  # Proceed to the next frame
+    silent_seconds_count = 0.0
+    consecutive_silent_seconds_required = silent_seconds_required
+    vad = webrtcvad.Vad()
+    vad.set_mode(3)  # High aggressiveness
+    frame_length = 320  # need to override this for webrtcvad
+
+    try:
+        # Record audio until silence is detected
+        while silent_seconds_count < consecutive_silent_seconds_required:
+            frame = audio_stream.read(frame_length, exception_on_overflow=False)
+            frames.append(frame)
+            is_speech = vad.is_speech(frame, sample_rate)
+
+            if not is_speech:
+                silent_seconds_count += frame_length / sample_rate
+            else:
+                # Reset count if speech is detected
+                silent_seconds_count = 0
+    except KeyboardInterrupt:
+        print("Recording interrupted by user.")
+
     print("Recording stopped.")
     return b"".join(frames)
 
@@ -145,6 +161,38 @@ def transcribe_audio(client, audio_data):
     else:
         print("No transcription results.")
         return None
+
+
+def send_text_to_gemini(user_input, previous_texts, timestamp):
+    # Combining past texts as context
+    context = " ".join(previous_texts)
+
+    # Adding system message
+    system_message = f"""System Message - Your identity: Gemini, you are a smart, kind, and helpful AI assistant.
+    You are at the Vancouver Cloud Summit and have the following information about the event: {cloud_summit_information}
+
+    Never respond with code or markdown. Respond only with text as if it is a conversation.
+    """
+
+    # Initializing Gemini model
+    model = genai.GenerativeModel("gemini-1.5-pro-latest")
+
+    # Sending image and text instructions to the model
+    prompt = f"{system_message}\nGiven the context: {context} and the current time: {timestamp}, please respond to the following message without repeating the context. Message: {user_input}"
+
+    try:
+        config = genai.GenerationConfig(max_output_tokens=1000)
+        response = model.generate_content(
+            [prompt], stream=True, generation_config=config
+        )
+        response.resolve()
+        # Returning the generated text
+        return response.text
+    except BlockedPromptException:
+        print(
+            "AI response was blocked due to safety concerns. Please try a different input."
+        )
+        return "AI response was blocked due to safety concerns."
 
 
 def text_to_speech_google(text, client):
@@ -171,118 +219,11 @@ def text_to_speech_google(text, client):
     # Loading the MP3 file
     sound = AudioSegment.from_mp3("./temp/output.mp3")
     # Adding a small amount of silence at the beginning
-    silence = AudioSegment.silent(duration=300)  # 300 milliseconds of silence
-    sound_with_silence = silence + sound  # Prepending silence to the sound
+    silence = AudioSegment.silent(duration=300)
+    sound_with_silence = silence + sound
 
     # Playing the sound
     play(sound_with_silence)
-
-
-def wrap_text(text, line_length):
-    """Function to wrap text to the specified length."""
-    words = text.split(" ")
-    lines = []
-    current_line = ""
-
-    for word in words:
-        if len(current_line) + len(word) + 1 > line_length:
-            lines.append(current_line)
-            current_line = word
-        else:
-            current_line += " " + word
-
-    lines.append(current_line)  # Adding the last line
-    return lines
-
-
-def add_text_to_frame(frame, text):
-    # Wrapping text every 70 characters
-    wrapped_text = wrap_text(text, 70)
-
-    # Getting the height and width of the frame
-    height, width = frame.shape[:2]
-
-    font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 1.0
-    color = (255, 255, 255)  # White color for contrast
-    outline_color = (0, 0, 0)  # Black outline for better visibility
-    thickness = 2  # Thickness of the font
-    outline_thickness = 4  # Increased outline thickness for better legibility
-    line_type = cv2.LINE_AA  # Anti-aliased line for smoother font edges
-
-    # Adding each line of text to the image
-    for i, line in enumerate(wrapped_text):
-        position = (10, 30 + i * 30)  # Adjusting the position of each line (larger gap)
-
-        # Drawing the outline of the text
-        cv2.putText(
-            frame,
-            line,
-            position,
-            font,
-            font_scale,
-            outline_color,
-            outline_thickness,
-            line_type,
-        )
-
-        # Drawing the text
-        cv2.putText(
-            frame, line, position, font, font_scale, color, thickness, line_type
-        )
-
-
-def save_frame(frame, filename, directory="./frames"):
-    # Create the directory if it does not exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    # Creating the path for the filename
-    filepath = os.path.join(directory, filename)
-    # Saving the frame
-    cv2.imwrite(filepath, frame)
-
-
-def save_temp_frame(frame, filename, directory="./temp"):
-    # Create the directory if it does not exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    # Creating the path for the filename
-    filepath = os.path.join(directory, filename)
-    # Saving the frame
-    cv2.imwrite(filepath, frame)
-    return filepath  # Returning the path of the saved file
-
-
-def send_frame_with_text_to_gemini(
-    frame, previous_texts, timestamp, user_input, client
-):
-    temp_file_path = save_temp_frame(frame, "temp.jpg")
-    img = PIL.Image.open(temp_file_path)
-
-    # Combining past texts as context
-    context = " ".join(previous_texts)
-
-    # Adding system message
-    system_message = f"""System Message - Your identity: Gemini, you are a smart, kind, and helpful AI assistant.
-    You are at the Vancouver Cloud Summit and have the following information about the event: {cloud_summit_information}
-    """
-
-    # Initializing Gemini model
-    model = client.GenerativeModel("gemini-pro-vision")
-
-    # Sending image and text instructions to the model
-    prompt = f"{system_message}\nGiven the context: {context} and the current time: {timestamp}, please respond to the following message without repeating the context, using no more than 20 words. Message: {user_input}"
-
-    try:
-        response = model.generate_content([prompt, img], stream=True)
-        response.resolve()
-        # Returning the generated text
-        return response.text
-    except BlockedPromptException:
-        print(
-            "AI response was blocked due to safety concerns. Please try a different input."
-        )
-        return "AI response was blocked due to safety concerns."
 
 
 def main():
@@ -307,12 +248,9 @@ def main():
     )
 
     genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
     # Initializing Google Cloud TTS API client
     tts_client = texttospeech.TextToSpeechClient()
-
-    video = cv2.VideoCapture(0)
-    if not video.isOpened():
-        raise IOError("Could not open the camera.")
 
     try:
         previous_texts = deque(maxlen=100)
@@ -327,43 +265,23 @@ def main():
 
                 # Detecting wake word using Porcupine
                 keyword_index = porcupine.process(pcm)
-                if keyword_index >= 0:  # If wake word is detected
+                if keyword_index >= 0:
                     print("Wake word detected!")
-                    start_time = time.time()  # Recording the current time
 
-                    # Continuing the process for 30 seconds after detecting wake word
-                    while True:  # Changing to an infinite loop
-                        current_time = time.time()
-                        # Checking if 30 seconds have passed
-                        if current_time - start_time >= 30:
-                            break  # Exiting the loop if 30 seconds have passed
-
-                        # Recording voice input and converting it to text
+                    # Continuing the process until silence is detected
+                    while True:
                         audio_data = record_audio(
-                            audio_stream,
-                            porcupine.sample_rate,
-                            porcupine.frame_length,
-                            5,
+                            audio_stream, porcupine.sample_rate, porcupine.frame_length
                         )
                         user_input = transcribe_audio(speech_client, audio_data)
 
                         # Processing if there is voice input
-                        if user_input:  # If there is voice input
-                            start_time = current_time  # Resetting the timer
+                        if user_input:
+                            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                            # Grabbing a new frame from the camera right after recording audio
-                            success, frame = video.read()
-                            if not success:
-                                print("Failed to grab a new frame.")
-                                break
-
-                            timestamp = datetime.now().strftime(
-                                "%Y-%m-%d %H:%M:%S"
-                            )  # Getting the current timestamp
-
-                            # Sending frame and user input to Gemini AI model and generating a response
-                            generated_text = send_frame_with_text_to_gemini(
-                                frame, previous_texts, timestamp, user_input, genai
+                            # Sending user input to Gemini AI model and generating a response
+                            generated_text = send_text_to_gemini(
+                                user_input, previous_texts, timestamp
                             )
                             print(
                                 f"Timestamp: {timestamp}, Generated Text: {generated_text}"
@@ -374,20 +292,12 @@ def main():
                                 f"Timestamp: {timestamp}\nUser Message: {user_input}\nYour Response: {generated_text}\n"
                             )
 
-                            # Adding the generated text to the frame
-                            text_to_add = f"{timestamp}: {generated_text}"
-                            add_text_to_frame(frame, text_to_add)
-
-                            # Saving the frame
-                            filename = f"{timestamp}.jpg"
-                            save_frame(frame, filename)  # Saving as an image
-
                             # Converting AI response to speech and playing it
                             text_to_speech_google(generated_text, tts_client)
 
-                        else:  # If there is no voice input
-                            print("No user input, exiting the loop.")
-                            break  # Exiting the loop
+                        else:
+                            print("Waiting for the keyword...")
+                            break
 
             except IOError as e:
                 if e.errno == pyaudio.paInputOverflowed:
@@ -398,13 +308,12 @@ def main():
                         audio_stream.start_stream()
                 else:
                     raise e
-
+    except KeyboardInterrupt:
+        print("\nExiting the program")
     finally:
         audio_stream.close()
         pa.terminate()
         porcupine.delete()
-        video.release()
-        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
